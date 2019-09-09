487d7a6b814570d8030f4460b688e338e7707d25
Denislav Denov <42144642+denislavdenov@users.noreply.github.com>
2019-09-09 08:46:33 +0300

    Update main.tf

--- dir2/main.tf
+++ dir2/main.tf
@@ -7,3 +7,8 @@ module "randomname" {
   source  = "app.terraform.io/ddenov/randomname/local"
   version = "0.0.3"
 }
+
+  module "randomname3" {
+  source  = "app.terraform.io/ddenov/randomname/local"
+  version = "0.0.3"
+}
