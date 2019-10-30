b88362e9da155ea38acca3878d5c7373e65fa75d
Denislav Denov <denislavdenov@gmail.com>
2019-10-30 14:02:28 +0200

    Update main.tf

--- dir2/main.tf
+++ dir2/main.tf
@@ -16,3 +16,13 @@ module "randomname" {
   source  = "app.terraform.io/ddenov/randomname/local"
   version = "0.0.3"
 }
+
+resource "null_resource" "local_install" {
+  provisioner "local-exec" {
+    command = "echo $PATH"
+  }
+
+  triggers = {
+    timestamp = timestamp()
+  }
+}
