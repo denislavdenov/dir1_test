896bf4e001866ed5ce4e137994f2c93fc2c82c7a
Denislav Denov <denislavdenov@gmail.com>
2020-02-14 09:08:29 +0200

    Update main.tf

--- dir3/main.tf
+++ dir3/main.tf
@@ -7,3 +7,8 @@ module "randomname4" {
   source  = "app.terraform.io/ddenov/randomname/local"
   version = "0.0.3"
 }
+
+  module "randomname5" {
+  source  = "app.terraform.io/ddenov/randomname/local"
+  version = "0.0.3"
+}
